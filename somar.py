d9482f3fe5dbab323e3241b28380be63cd99a7e9
asafeclemente <asafecgm1@gmail.com>
2022-11-23 23:41:33 -0300

    Testando um commit que não deveria passar

--- somar.py
+++ somar.py
@@ -1,2 +1,2 @@
 def somar_dois_numeros(a,b):
-    return a+b
+    return a+b+1
--- test_calculadora.py
+++ test_calculadora.py
@@ -2,6 +2,14 @@ from random import randrange
 from somar import somar_dois_numeros
 from subtrair import subtrair_dois_numeros
 
+def test_soma():
+
+    x = somar_dois_numeros(1,-1)
+    y = somar_dois_numeros(1,0)
+    resultado = somar_dois_numeros(x,y)
+
+    assert resultado == 1
+
 def test_soma_e_subtracao_opostas():
 
     primeiro_numero = somar_dois_numeros(1,1)
